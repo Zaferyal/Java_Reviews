a50e337ca36be84c109969441f4ac501f696ca30
Muhtar <muhtar.mahmut0519@gmail.com>
2022-10-22 12:12:29 -0400

    Week04 Review

--- src/week04_review/StringMethodsPractice.java
+++ src/week04_review/StringMethodsPractice.java
@@ -33,6 +33,53 @@ public class StringMethodsPractice {
 
         System.out.println("indexOfSecondO = " + indexOfSecondO);
 
+
+        System.out.println("---------------------------------------------------------------");
+
+
+        String s1 = "BATCH 28";
+
+        s1 = s1.toLowerCase();// "batch 28"
+
+        System.out.println("s1 = " + s1);
+
+        System.out.println("---------------------------------------------------------------");
+
+        String s2 = "I live in Virginia, I love Virginia";
+
+       // s2 = s2.replace("Virginia", "New York");
+        //s2 = s2.replaceFirst("Virginia", "New York");
+        s2 = s2.replaceFirst("love Virginia", "love New York");
+
+        System.out.println(s2);
+
+        System.out.println("---------------------------------------------------------------");
+
+
+        String s3 = "Batch 28 students";
+
+        String t1 = s3.substring(0, 8);
+
+        System.out.println(t1);
+
+        String t2 = s3.substring(  s3.indexOf('s') );
+
+        System.out.println(t2);
+
+        System.out.println("---------------------------------------------------------------");
+
+        String name = "mUhTaR";
+
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        //name =  ( "" + name.charAt(0) ).toUpperCase()    + name.substring(1).toLowerCase();
+
+        System.out.println(name);
+
+
+
+
+
+
     }
 
 
