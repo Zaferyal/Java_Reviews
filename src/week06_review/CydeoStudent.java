e8151d3e8242ac0c30f4216ed30adaff96b87c6a
Muhtar <muhtar.mahmut0519@gmail.com>
2022-11-05 12:56:10 -0400

    Week06 Review

---
+++ src/week06_review/CydeoStudent.java
@@ -0,0 +1,21 @@
+package week06_review;
+
+public class CydeoStudent {
+
+    public String name;
+    public int age;
+    public char gender;
+    public boolean isMarried, isEmployed;
+    public int batchNumber;
+    public String programmingLanguage;
+    public String groupName;
+
+   // public static String schoolName = "Cydeo";
+
+}
+
+/*
+In each object, user should be able to store the:
+    name, age, gender, isMarried, isEmployed, batch, programming language, and group name information of the Cydeo student
+
+ */
