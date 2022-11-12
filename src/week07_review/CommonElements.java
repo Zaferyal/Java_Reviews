8b44c5a2c4a1ec22539be94ce8096c93861c82ec
Muhtar <muhtar.mahmut0519@gmail.com>
2022-11-12 12:54:45 -0500

    Week07 Review

--- src/week07_review/CommonElements.java
+++ src/week07_review/CommonElements.java
@@ -4,24 +4,19 @@ public class CommonElements {
 
     public static void main(String[] args) {
 
-        String[] arr1 = {"Python", "Java", "C#", "Wooden Spoon", "Swift", "Java"};
+        String[] arr1 = {"Python", "Java", "C#", "Wooden Spoon", "Swift"};
         String[] arr2 = {"Selenium", "SQL", "Java", "API", "Jenkins", "Wooden Spoon"};
-
-        String result = "";
 
         for (String element : arr1) { // element: each element of first array
 
             for (String each : arr2) { // each: each element of second array
                 if( each.equals( element) ){ // if the element of first array is included in the second array
-                    if(!result.contains(element)){
-                        result += element+"\n";
-                    }
+                    System.out.println(each);
                 }
             }
 
         }
 
-        System.out.println(result);
 
 
 
