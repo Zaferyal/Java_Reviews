611550b9afd83a8d69c62d6a74fc8d00eefc0fb8
Muhtar <muhtar.mahmut0519@gmail.com>
2022-11-26 11:31:49 -0500

    Week09 Review

---
+++ src/week09_review/BankOfAmerica.java
@@ -0,0 +1,16 @@
+package week09_review;
+
+import java.time.LocalDate;
+
+public class BankOfAmerica {
+
+    public static void main(String[] args) {
+
+        Tester tester1 = new Tester("Josh", 'M', LocalDate.of(1985, 4, 5) , "A01", "QA", 100000);
+
+        System.out.println(tester1);
+
+
+    }
+
+}
---
+++ src/week09_review/Tester.java
@@ -0,0 +1,51 @@
+package week09_review;
+
+import java.time.LocalDate;
+
+public class Tester {
+
+    public String name;
+    public char gender;
+    public LocalDate DOB;
+    public int age;
+    public String employeeId;
+    public String jobTitle;
+    public double salary;
+
+    public Tester(String name, char gender, LocalDate DOB, String employeeId, String jobTitle, double salary){
+        this.name = name;
+        this.gender = gender;
+        this.DOB = DOB;
+        age = LocalDate.now().getYear() - DOB.getYear();
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    /*
+    public static void Tester(){
+        System.out.println("Regular Method");
+    }
+
+
+    public static void main(String[] args) {
+
+        new Tester();
+
+    }
+*/
+
+
+
+}
+
+/*
+1. create a class called Tester
+		    Attributes:
+		        name, gender, dateOfBirth, age, employeeID, JobTitle, Salary
+
+		    Add a constructor that can set all the fields
+
+		    Actions:
+		       smokeTesting(),  creatingTicket(), dailyStandUp(), toString()
+ */
