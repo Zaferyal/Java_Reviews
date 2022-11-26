c8de360b4b0d5537f2ef6f01880fc22f679433de
Muhtar <muhtar.mahmut0519@gmail.com>
2022-11-26 12:53:48 -0500

    Week09 Review

---
+++ src/week09_review/ScrumTeam.java
@@ -0,0 +1,28 @@
+package week09_review;
+
+public class ScrumTeam {
+}
+
+/*
+3. create a class called ScrumTeam
+           	Attributes:
+                  PO, BA, SM (for storing their names ONLY)
+                  testers, developers, daysOfSprint
+
+            Add a constructor that can set the PO, BA, SM and daysOfSprint
+
+         	Actions:
+                addTester(Tester tester): adds the given tester to the testers ArrayList
+
+                addTesters(Tester[] testers): adds the given testers to the testers ArrayList
+
+                addDeveloper(Developer developer): adds the given developer to the developers ArrayList
+
+                addDevelopers(Developer[] developers): adds the given developers to the developers ArrayList
+
+                removeTester(long employeeID): removes the given tester from the testers ArrayList
+
+                removeDeveloper(long employeeID): removes the developer from the developers ArrayList
+
+                toString(): prints number of tester,& developers,  PO name, SM name, BA name, and daysOfSprint
+ */
