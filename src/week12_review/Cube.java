ce3f8f836a7d8303d9fffa06756b69dcf57bb250
Muhtar <Muhtar.Mahmut0519@gmail.com>
2022-12-17 12:28:19 -0500

    Week12 Review

--- src/week12_review/Cube.java
+++ src/week12_review/Cube.java
@@ -4,39 +4,20 @@ import static java.lang.Math.*;
 
 public class Cube extends Shape {
 
-    private double length, width,  height;
+    private double side;
 
-    public Cube( double length, double width, double height) {
+    public Cube( double side) {
         super("Cube");
-        setLength(length);
-        setWidth(width);
-        setHeight(height);
+        setSide(side);
     }
 
-    public double getLength() {
-        return length;
+    public double getSide() {
+        return side;
     }
 
-    public void setLength(double length) {
-        this.length = length;
+    public void setSide(double side) {
+        this.side = side;
     }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
 
     @Override
     public double calc_area() {
